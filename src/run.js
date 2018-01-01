b6ce457d7b0749089d9cf59b5ea83d063db2c784
Oyed <tom@mosey.io>
2018-01-01 16:58:17 +0000

    Added exit cleanup.

--- src/run.js
+++ src/run.js
@@ -4,3 +4,26 @@ const client = require('./client');
 const { parse } = require('./parser');
 
 client.on('message', msg => parse(msg));
+
+// Tell the process not to exit straight away.
+process.stdin.resume();
+
+let killed = false;
+
+const exit = () => {
+    if (killed) {
+        return;
+    }
+
+    killed = true;
+    client.destroy().then(() => {
+        process.exit();
+    });
+};
+
+// Register the Listeners for different exit events.
+process.on('exit', () => exit());
+process.on('SIGINT', () => exit());
+process.on('SIGUSR1', () => exit());
+process.on('SIGUSR2', () => exit());
+process.on('uncaughtException', () => exit());
