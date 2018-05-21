a8d645bcebbd0b4756ff00f4779e7c176c3d4277
Oyed <tom@mosey.io>
2018-05-21 21:44:03 +0800

    Added Activity because why not.

--- src/client.js
+++ src/client.js
@@ -2,7 +2,11 @@ const Discord = require('discord.js');
 const Dbl = require('dblapi.js');
 
 const client = new Discord.Client();
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN).then(() => {
+    client.user.setActivity('Cryptocurrencies', {
+        type: 'WATCHING',
+    });
+});
 
 const dbl = new Dbl(process.env.DISCORDBOTS_TOKEN, client);
 
