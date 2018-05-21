f5bf943dd08502b47b326dc3725227cfc6cf6bf5
Oyed <tom@mosey.io>
2018-05-21 18:47:29 +0800

    Added support for DiscordBots.org.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
+const Dbl = require('dblapi.js');
 
 const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
+
+const dbl = new DBL(process.env.DISCORDBOTS_TOKEN, client);
 
 module.exports = client;
