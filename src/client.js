87edbd7a884e016c9f371f8dd9e220bd2d561f46
Oyed <tom@mosey.io>
2018-05-21 18:48:34 +0800

    Fixed typo.

--- src/client.js
+++ src/client.js
@@ -4,6 +4,6 @@ const Dbl = require('dblapi.js');
 const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
 
-const dbl = new DBL(process.env.DISCORDBOTS_TOKEN, client);
+const dbl = new Dbl(process.env.DISCORDBOTS_TOKEN, client);
 
 module.exports = client;
