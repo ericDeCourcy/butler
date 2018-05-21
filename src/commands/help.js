7322d3851adea7389aea6b08a873c26a89d6805f
Oyed <tom@mosey.io>
2018-05-21 23:38:26 +0800

    Remove useless help header and make the command a lil sassier.

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,14 +4,10 @@ class Help extends Command {
 
     execute(msg, params = []) {
         const prefix = process.env.COMMAND_PREFIX;
-        const content = `**CryptoButler Help**
-
----
-
-\`${prefix} price [id]\` - Fetches data from CoinMarketCap for the given ID.
+        const content = `\`${prefix} price [id]\` - Fetches data from CoinMarketCap for the given ID.
 \`${prefix} convert [amount] [from] [to]\` - Converts a given amount of \`from\` to currency \`to\`.
 \`${prefix} balance [ticker] [address]\` - Returns the balance of the given Wallet. Supports \`eth\`, \`btc\` and \`tokens\` (ERC-20).
-\`${prefix} help\` - Prints this text.
+\`${prefix} help\` - I'll give you 10 guesses.
 
 ---
 
